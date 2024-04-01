27a9069a4e8255531fd280138d9d5e033325797f
Donghwan Oh <imodh97@gmail.com>
2024-04-02 00:00:00 +0900

    #3.8

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2">
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col ring ring-transparent transition-shadow md:flex-row gap-2 *:outline-none has-[:invalid]:ring-red-200 ">
         <input
-          className="w-ful rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          className="w-full rounded-full h-12 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="text"
-          placeholder="Search here..."
+          required
+          placeholder="Email address"
         />
-        <button className="bg-black text-white py-2 rounded-full focus:scale-90 outline-none  active:scale-90 transition-transform font-medium">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required.
+        </span>
+        <button className="bg-black text-white py-2 rounded-full   active:scale-90 transition-transform font-medium md:px-10 peer-invalid:bg-red-200">
+          Login
         </button>
       </div>
     </main>
